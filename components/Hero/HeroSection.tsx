c1dc18d90e6cb42dd2ec20a16bc69e5eebaae843
Jairo <jairoavilam@gmail.com>
2025-09-17 13:55:31 -0500

    bug on heroSection fixed

--- components/Hero/HeroSection.tsx
+++ components/Hero/HeroSection.tsx
@@ -45,7 +45,7 @@ export const HeroSection: React.FC<HeroSectionProps> = ({
           <Button
             testId="cta-secondary"
             label="Ver Portfolio"
-            href="/portfolio"
+            href="/products"
             variant="secondary"
           />
         </div>
